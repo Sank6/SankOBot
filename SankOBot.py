76a570f20a9382e2ed9940ae4259c0003da1d106
Guru Ankrad <25014241+Sank6@users.noreply.github.com>
2017-12-30 17:37:36 +0000

    Update SankOBot.py

--- SankOBot.py
+++ SankOBot.py
@@ -110,15 +110,15 @@ async def on_message(message):
             # ping with "d"
 
         if message.content.startswith('%ping'):
-            pingtime = time.time()
-            pingms = await client.send_message(message.channel, "Pinging...")
-            ping = time.time() - pingtime
-            await client.edit_message(pingms, "Pong! `%.01f seconds`" % ping)
+            t1 = time.perf_counter()
+            ponger = await client.send_message(message.channel, 'Pinging...')
+            t2 = time.perf_counter()
+            await client.edit_message(ponger, "Pong! `{} ms`".format(round((t2 - t1) * 1000)))
         if message.content.startswith('%pingd'):
-            pingtime = time.time()
-            pingms = await client.send_message(message.channel, "Pinging...")
-            ping = time.time() - pingtime
-            await client.edit_message(pingms, "Pong! `%.01f seconds`" % ping)
+            t1 = time.perf_counter()
+            ponger = await client.send_message(message.channel, 'Pinging...')
+            t2 = time.perf_counter()
+            await client.edit_message(ponger, "Pong! `{} ms`".format(round((t2 - t1) * 1000)))
             await client.delete_message(message)
 
 
