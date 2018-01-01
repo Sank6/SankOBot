63d2a5ff78269e5647fd7ffbcd68419a81a09409
Guru Ankrad <25014241+Sank6@users.noreply.github.com>
2018-01-01 22:19:41 +0000

    Update servers_s.py

--- Levels/servers_s.py
+++ Levels/servers_s.py
@@ -1,11 +1,23 @@
-import sqlite3
+import psycopg2
+from urllib import parse
+import os
+
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ("DATABASE_URL"))
 
 
 def add_server(name, server_id, invite_link, member_count):
     list_of_ids = []
     list_of_server_ids = []
-    user = sqlite3.connect('server_s.db')
-    cursor = user.execute("SELECT ID, SERVER_ID from SERVERS")
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT ID, SERVER_ID from SERVERS")
     for row in cursor:
         list_of_ids.append(row[0])
         list_of_server_ids.append(row[1])
@@ -22,20 +34,43 @@ def add_server(name, server_id, invite_link, member_count):
 
 
 def add_server_tuple(values):
-    with sqlite3.connect('server_s.db') as db:
-        cursor = db.cursor()
-        sql = "INSERT INTO SERVERS (ID, NAME, SERVER_ID, INVITE, MEMBERS, REMOVED) values (?, ?, ?, ?, ?, 0)"
-        cursor.execute(sql, values)
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    cursor = conn.cursor()
+    sql = """
+     INSERT INTO servers (ID, NAME, SERVER_ID, INVITE, MEMBERS, REMOVED)
+     VALUES (%s, %s, %s, %s, %s, 0);
+     """
+    cursor.execute(sql, values)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 def removed_from_server(server_id):
-    user = sqlite3.connect('server_s.db')
-    cursor = user.execute("SELECT ID, SERVER_ID from SERVERS")
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT ID, SERVER_ID from SERVERS")
     for row in cursor:
         if str(row[1]) == str(server_id):
             id_ = row[0]
         else:
             pass
-    cursor.execute('''UPDATE SERVERS set REMOVED = ? where ID = ? ''',
-                   ("1", str(id_)))
-    user.commit()
+    cursor.execute("UPDATE SERVERS set REMOVED = %s where ID = %s ", ("1", str(id_)))
+    # cursor.execute("DELETE from servers where ID = %s", (str(id_,)))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
